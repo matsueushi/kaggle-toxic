c338b7b7cc6ee16aa278344b833383638982c2e3
matsueushi <sinxcosxtanx@gmail.com>
2018-03-05 23:30:41 +0900

    logistic-regression

--- src/kaggle-toxic.py
+++ src/kaggle-toxic.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 from keras.preprocessing.text import text_to_word_sequence, Tokenizer
 from gensim.parsing.preprocessing import preprocess_string
 from gensim import corpora
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 
 # %%
@@ -23,17 +26,17 @@ test.head()
 
 
 # %%
-LIST_CLASSES = ['toxic', 'severe_toxic',
-                'obscene', 'threat', 'insult', 'identity_hate']
-ind = np.arange(len(LIST_CLASSES))
+LABELS = ['toxic', 'severe_toxic',
+          'obscene', 'threat', 'insult', 'identity_hate']
+ind = np.arange(len(LABELS))
 plt.style.use('bmh')
 fix, ax = plt.subplots()
-sum_labels = [train[l].sum() for l in LIST_CLASSES]
+sum_labels = [train[l].sum() for l in LABELS]
 plot_bars = plt.bar(ind, sum_labels)
 ax.set_title('Toxicity types')
 ax.yaxis.grid()
 ax.set_xticks(ind)
-ax.set_xticklabels(LIST_CLASSES)
+ax.set_xticklabels(LABELS)
 
 
 # %%
@@ -44,9 +47,9 @@ for df in [train, test]:
 
 
 # %%
-fig, ax = plt.subplots(nrows=len(LIST_CLASSES),
+fig, ax = plt.subplots(nrows=len(LABELS),
                        sharex=True, sharey=True, figsize=(10, 20))
-for i, c in enumerate(LIST_CLASSES):
+for i, c in enumerate(LABELS):
     not_toxic = train[train[c] == 0]
     toxic = train[train[c] == 1]
     print(len(not_toxic), len(toxic))
@@ -84,7 +87,7 @@ all_word_list = get_word_list(
 
 not_toxic_word_list = {}
 toxic_word_list = {}
-for c in LIST_CLASSES:
+for c in LABELS:
     not_toxic = train[train[c] == 0]
     toxic = train[train[c] == 1]
     not_toxic_word_list[c] = get_word_list(not_toxic['preprocessed_comment_text'],
@@ -97,7 +100,7 @@ for c in LIST_CLASSES:
 plt.figure(figsize=(15, 4))
 plt.title('Top 50 word frequencies(All)')
 all_word_list[:50].plot.bar()
-for c in LIST_CLASSES:
+for c in LABELS:
     plt.figure(figsize=(15, 4))
     plt.title('Top 50 word frequencies(Class=' + c + ', Positive)')
     not_toxic_word_list[c][:50].plot.bar()
@@ -105,14 +108,44 @@ for c in LIST_CLASSES:
     plt.title('Top 50 word frequencies(Class=' + c + ', Negative)')
     toxic_word_list[c][:50].plot.bar()
 
-
 # %%
 COMMON_SAVE_COLUMNS = ['id', 'preprocessed_comment_text',
                        'word_count', 'preprocessed_word_count']
-test_save_columns = LIST_CLASSES[:]
+test_save_columns = LABELS[:]
 test_save_columns.extend(COMMON_SAVE_COLUMNS)
 
 train[test_save_columns].to_csv('../input/train_prepro.csv')
 test[COMMON_SAVE_COLUMNS].to_csv('../input/test_prepro.csv')
 train[test_save_columns][:100].to_csv('../input/train_prepro_first100.csv')
 test[COMMON_SAVE_COLUMNS][:100].to_csv('../input/test_prepro_first100.csv')
+
+
+# %%
+# TfidfVectorizer
+tfidf_vec = TfidfVectorizer(max_features=20000, min_df=2)
+train_dtm = tfidf_vec.fit_transform(train['preprocessed_comment_text'])
+test_dtm = tfidf_vec.transform(test['preprocessed_comment_text'])
+
+
+# %%
+print(type(train_dtm))
+print(train_dtm[0])
+
+
+# %%
+submission = pd.DataFrame(test['id'])
+logistic_reg = LogisticRegression(C=5.0)
+for c in LABELS:
+    train_y = train[c]
+    logistic_reg.fit(train_dtm, train_y)
+    pred_train_y = logistic_reg.predict(train_dtm)
+    print(c, accuracy_score(train_y, pred_train_y))
+    pred_test_y = logistic_reg.predict_proba(test_dtm)[:, 1]
+    submission[c] = pred_test_y
+
+
+# %%
+
+
+# %%
+submission.to_csv('submission_logistic_reg.csv', index=False)
